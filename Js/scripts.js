b05ad994ca7ac2e67c159e3c64b8e14b063fcd59
Estelope <elopez14567@gmail.com>
2023-06-29 20:21:18 -0600

    show logic function progress and touchup README

---
+++ Js/scripts.js
@@ -0,0 +1,32 @@
+function bedazzleNumberInput(num){
+  let numberArray = [num];
+
+  numberArray.foreach(function (num, index){
+  if((numberArray).includes(num['1']))
+console.log(numberArray);
+    return indexof(numberArray);
+})
+
+
+ // for(let i=number[i].length; i>=0; i-= 1) nested for loop that will generate numbered list index
+ 
+
+
+
+
+
+
+
+console.log(i);
+console.log(indexof(numberArray))
+  return indexof(numberArray);
+  return numberArray
+}
+
+
+
+
+
+
+
+//if Array.includes(1)
